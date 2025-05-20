9e1d6f26d72fa2e0503e26ed15c8651d158c4fb9
Rebecca McGirr <you@example.com>
2025-05-20 13:12:07 -0700

    first commit

---
+++ JS5WA08.js
@@ -0,0 +1,8 @@
+/*
+Week 08 Coding Assignment Steps:
+Create a menu app as seen in this week’s video. What you create is up to you as long as it meets the following requirements:
+    1) Use at least one array.
+    2) Use at least two classes.
+    3) Your menu should have the options to create, view, and delete elements.
+*/
+
